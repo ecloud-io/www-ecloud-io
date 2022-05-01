ec5927560a708c4edc0ba6a18f4efe369ca69dcd
Adam Upholzer <adam@upholzer.dev>
2022-05-01 16:20:55 -0400

    Theme Update

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,12 @@
-import Head from 'next/head'
+import React, { Component } from "react";
+import Router from "next/router";
 
-import Post from '../components/post'
+export default class Index extends Component {
+  componentDidMount = () => {
+    Router.push("/presentation");
+  };
 
-export async function getStaticProps() {
-  // fetch list of posts
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_page=1'
-  )
-  const postList = await response.json()
-  return {
-    props: {
-      postList,
-    },
+  render() {
+    return <div />;
   }
-}
-
-export default function IndexPage({ postList }) {
-  return (
-    <main>
-      <Head>
-        <title>Home page</title>
-      </Head>
-
-      <h1>List of posts</h1>
-
-      <section>
-        {postList.map((post) => (
-          <Post {...post} key={post.id} />
-        ))}
-      </section>
-    </main>
-  )
 }
